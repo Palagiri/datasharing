02bb6962493bd67f01757641e12b90a39e4562b2
Hafeez <p.hafeez@gmail.com>
2014-06-26 15:42:09 -0400

    editing the pactise file on june 26.

--- practise-week1.R
+++ practise-week1.R
@@ -32,4 +32,43 @@ dateDownloaded <- date()
 cameradataXLSX = readWorksheet (object="cameras.xlsx", sheet=1, header=TRUE)
 #I am having problem in reading the xlsx file, have tried other options as well.
 
-#Reading the 
+#Reading the in XML format
+library (XML)
+fileUrl = "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse( fileUrl, useInternal=TRUE )
+rootNode <- xmlRoot (doc)
+xmlName (rootNode)
+names(rootNode)
+dateDownloaded <- date()
+#Now we have all the XML file into the memory and we can access this as how we acess a list.
+rootNode[[1]]
+rootNode[[3]]
+rootNode[[1]][[1]]
+rootNode[[1]][[1]][[1]]
+rootNode
+
+#Apply sapply function on the rootNode - xml document and trying to extract all the data which is in the 
+#tags.
+xmlSApply ( rootNode, FUN=xmlValue)
+
+
+#data.table()
+install.packages("data.table")
+library (data.table)
+
+?data.table
+DT = data.table(x=rnorm (9), y=rep(c("a", "b", "c"),each = 3), z = dnorm(9))
+head(DT, 3)
+DT[DT$y == "a"]
+DT[c(2,3),]
+DT[,c(2,3)] #In data.table subsetting column its not same as dataframes.
+tables()
+
+DF = data.frame( x = rnorm(9), y = rep(c("a", "b", "c"), each = 3), z = dnorm (9))
+DF
+DF[c(2,3), ]
+DF[,c(2,3)]
+k = {print (10); 5}
+print (k)
+ls()
+k
