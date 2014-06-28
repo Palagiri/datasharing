61bf70a539eb70e4d6d7159183ccc708396fdeb2
Hafeez <p.hafeez@gmail.com>
2014-06-28 10:20:22 -0400

    added some more info

--- subsetting-pract.R
+++ subsetting-pract.R
@@ -32,3 +32,46 @@ df[is.na(df)]
 
 #If we want to print all the elements except NA
 df[!is.na(df)]
+
+#sorting elements in the data.frame
+sort (df$var1, decreasing=TRUE)
+sort (df$var2, na.last=TRUE)
+
+#order the data.frame w.r.t one column 
+df [order(df$var1),]
+#we cannot just use order function to print the order of elements in a particular column
+order(df$var1)# does not work
+
+#we can order data.frame using multiple columns
+df[order(df$var1, df$var3),]
+
+#there is this new package plyr that we can use for sorts 
+
+library(plyr)
+#arranges the data.frame w.r.t var1 in ascending order
+arrange(df, var1)
+#arranges the data.frame w.r.t var1 in descending order
+arrange (df, desc(var1))
+#arranges the data.frame w.r.t var1 in ascending order
+arrange (df, -desc(var1))
+
+#Adding a new column to the data.frame
+df$var4 <- rnorm (5)
+df
+#using cbind command, the following command binds the new elements to the right side
+df <- cbind (df, "var5"=dnorm(5))
+df
+
+df <- cbind ("var0" = pnorm(5), df)
+df
+
+#we can use rbind to add the rows 
+df <- rbind (df, rnorm(6))
+df
+
+
+
+
+              
+              
+              
